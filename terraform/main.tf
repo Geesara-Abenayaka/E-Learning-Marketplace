04abf0583814acd70d7f97d0001fe1ddae76e99a
Geesara-Abenayaka <Abefamily80@gmail.com>
2026-02-07 17:47:01 +0530

    changes have been done to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Security Group
 resource "aws_security_group" "allow_ssh" {
-  name = "allow_ssh"
+  name        = "allow_ssh"
+  description = "Allow SSH from anywhere"
 
   ingress {
     from_port   = 22
@@ -18,10 +20,16 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # Add tags so AWS console shows the name
+  tags = {
+    Name = "allow_ssh"
+  }
 }
 
+# EC2 Instance
 resource "aws_instance" "docker_vm" {
-  ami           = "ami-0b9064170e32bde34" # Ubuntu 22.04 (us-east-2)
+  ami           = "ami-0b9064170e32bde34" # Ubuntu 22.04
   instance_type = "t3.small"
   key_name      = "ansible"
 
